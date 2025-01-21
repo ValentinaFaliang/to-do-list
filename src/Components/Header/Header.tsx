81d5242aa2d10383426c183183a3cb4e498cdad8
Faliang <rukatata@gmail.com>
2025-01-21 13:27:18 +0300

    marked up header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,13 +5,25 @@ import "./Header.css";
 export const Header = () => {
   return (
     <header className="header">
-      <div className="indicator__container">
-        <div className="indicator"></div>
-      </div>
-      <h1>To-Do List</h1>
-      <button>
-        <img src={checkIcon} alt="check mark" />
-      </button>
+      <section className="header__top">
+        <div className="header__top__left"></div>
+        <div className="header__top__right"></div>
+      </section>
+      <section className="header__middle">
+        <div className="indicator__container">
+          <div className="indicator">
+            {/* процент на сколько выполнены задачи */}
+          </div>
+        </div>
+        <div className="title__container">
+          <h1>To-Do List</h1>
+        </div>
+        <div className="button__container">
+          <button>
+            <img src={checkIcon} alt="check mark" />
+          </button>
+        </div>
+      </section>
     </header>
   );
 };
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -4,7 +4,19 @@ import "./Main.css";
 export const Main = () => {
   return (
     <main className="main">
-      <h1>main</h1>
+      <div className="main__container">
+        <div className="main__left-container">
+          <section className="today-tasks"></section>
+          <section className="week-tasks"></section>
+        </div>
+        <div className="main__right-container">
+          <section className="tasks-status">
+            <section className="completed-tasks"></section>
+            <section className="pending-tasks"></section>
+          </section>
+          <section className="notes"></section>
+        </div>
+      </div>
     </main>
   );
 };
