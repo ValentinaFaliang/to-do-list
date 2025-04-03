06aa09483c11c31f4dc97fbaa9f465d7144789ff
Faliang <rukatata@gmail.com>
2025-04-03 14:35:00 +0300

    added percentages and changed type of coloring background

--- src/Components/Indicator/Indicator.tsx
+++ src/Components/Indicator/Indicator.tsx
@@ -6,16 +6,19 @@ import { selectCompletionPercentage } from "../../store/task/taskSelectors";
 export const Indicator = () => {
   const completionPercentage = useSelector(selectCompletionPercentage);
   return (
-    <div className="indicator__container">
-      <div
-        className="progress"
-        style={
-          {
-            "--progress": `${completionPercentage.toFixed(0)}%`,
-          } as React.CSSProperties
-        }
-      />
-      <div className="indicator"></div>
+    <div
+      className="indicator__container"
+      data-progress={completionPercentage.toFixed(0)}
+    >
+      <div className="indicator">
+        <h3 className="percentage-progress">
+          {completionPercentage.toFixed(0)}%
+        </h3>
+        <div
+          className="progress"
+          style={{ height: `${completionPercentage}%` }}
+        />
+      </div>
     </div>
   );
 };
